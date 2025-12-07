24981a4777b17b972f21d192c9be721b29c88910
Rommel Dijon <bartmilhausky@gmail.com>
2025-12-07 08:28:22 -0800

    Updated ProtectedScreen.js file

--- components/ProtectedScreen.js
+++ components/ProtectedScreen.js
@@ -3,13 +3,23 @@ import { Text, View, StyleSheet } from 'react-native';
 import { AuthContext } from './AuthProvider';
 
 function ProtectedScreen() {
-  
+   const { user } = useContext(AuthContext);
+
+  if (!user) {
+    return <Text>Please log in to access this screen.</Text>;
+  }
+
+  if (user.role === 'admin') {
+    return (
+      <View style={styles.container}>
+        <Text>Access Granted. Welcome Admin!</Text>
+      </View>
+    );
+  }
 
   return (
-  <Text testID='accessDeniedMessage'>
-
-  </Text>
-  )
+    <Text testID='accessDeniedMessage'>Access Denied. Only admins can view this screen.</Text>
+  );
  }
 
 const styles = StyleSheet.create({
