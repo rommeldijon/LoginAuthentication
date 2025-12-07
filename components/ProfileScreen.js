2282c9fed4e7f0b05917085eebb97849a8b5147d
Rommel Dijon <bartmilhausky@gmail.com>
2025-12-07 08:25:09 -0800

    Update ProfieScreen.js

--- components/ProfileScreen.js
+++ components/ProfileScreen.js
@@ -3,10 +3,18 @@ import { Text, View, Button, StyleSheet } from 'react-native';
 import { AuthContext } from './AuthProvider';
 
 function ProfileScreen() {
+    const { user, logout } = useContext(AuthContext);
+
+  if (!user) {
+    return <Text>Please log in.</Text>;
+  }
 
   return (
     <View style={styles.container}>
      <View style={styles.text}></View>
+      <Text>Welcome, {user.username}!</Text>
+      <Text>Email: {user.email}</Text>
+      <Button title="Logout" onPress={logout} />
     </View>
   );
 }
