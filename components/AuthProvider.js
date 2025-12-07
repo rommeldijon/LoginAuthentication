7cc2a96adc44f8e503281f8620a447d56a0c3aa8
Rommel Dijon <bartmilhausky@gmail.com>
2025-12-07 08:21:10 -0800

    Update AuthProvider & LoginScreen .js files

--- components/AuthProvider.js
+++ components/AuthProvider.js
@@ -7,9 +7,24 @@ export const AuthContext = createContext();
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
 
+const login = (userData) => {
+    setUser(userData);
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
   return (
     <AuthContext.Provider value={{ user}}>
       {children}
     </AuthContext.Provider>
   );
 }
+
+
+ <AuthContext.Provider value={{ user,login,logout}}>
+      {children}
+    </AuthContext.Provider>
+
+    
--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,12 +1,26 @@
 import React, { useContext, useState } from 'react';
 import { Button, TextInput, View, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { AuthContext } from './AuthProvider';
 
 function LoginScreen() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  const { login } = useContext(AuthContext);
+const navigation = useNavigation(); // Access the navigation object
 
+  const handleLogin = () => {
+    if (username === 'admin' && password === 'admin123') {
+      login({ username: 'admin', email: 'admin@example.com', role: 'admin' });
+      navigation.navigate('Profile'); // Navigate to ProfileScreen after successful login
+    } else if (username === 'user' && password === 'user123') {
+      login({ username: 'user', email: 'user@example.com', role: 'user' });
+      navigation.navigate('Profile'); // Navigate to ProfileScreen after successful login
+    } else {
+      alert('Invalid credentials');
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -23,7 +37,7 @@ function LoginScreen() {
         secureTextEntry
         style={styles.input}
       />
-      <Button title="Login"/>
+      <Button title="Login" onPress={handleLogin} />
     </View>
   );
 }
